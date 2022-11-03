2e9cbd280a25fc99d96cf16fa3589e165d8c67d8
Gudenaf <xfitxrfrjat11>
2022-11-04 01:34:42 +0200

    Commit

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ $(window).scroll(function () {
     let parallaxScroll = $(window).scrollTop();
 
     $('.parallax__img1, .parallax__img2').css({
-        width: (100 - parallaxScroll/15) + "%",
-        height: (100 - parallaxScroll/15) + "%"
+        width: (100 - parallaxScroll / 15) + "%",
+        height: (100 - parallaxScroll / 15) + "%"
     });
 
     $('.parallax__img1').css({
-        right: (1 + parallaxScroll/10) + "%"
+        right: (1 + parallaxScroll / 10) + "%"
     });
 
     $('.parallax__img2').css({
-        left: (1 + parallaxScroll/10) + "%"
+        left: (1 + parallaxScroll / 10) + "%"
     });
 
     $('.main__title').css({
-        top: (100 - parallaxScroll/10) + "%",
+        top: (100 - parallaxScroll / 10) + "%",
     });
 });
